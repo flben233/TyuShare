1e8c69d57391373cc787efc64af84dffe26cfd54
ShirakawaTyu <1400070301@qq.com>
2023-08-21 00:26:50 +0800

    Init

--- src/jvmTest/java/Test.java
+++ src/jvmTest/java/Test.java
@@ -1,18 +1,6 @@
-import androidx.compose.runtime.Composable;
-import cn.hutool.core.swing.clipboard.ClipboardListener;
-import cn.hutool.core.swing.clipboard.ClipboardUtil;
-import cn.hutool.http.HttpUtil;
-import util.PackageScanUtil;
 
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.Transferable;
 
 public class Test {
     public static void main(String[] args) throws ClassNotFoundException {
-        System.out.println(PackageScanUtil.scan("view"));
-        Class<?> aClass = Class.forName("view.ConnectViewKt");
-        PackageScanUtil.handleMethodAnnotation(aClass.getDeclaredMethods(), Composable.class, ((method, annotation) -> {
-            System.out.println(method.getName());
-        }));
     }
 }
--- src/jvmTest/kotlin/Test.kt
+++ src/jvmTest/kotlin/Test.kt
@@ -1,7 +1,4 @@
-import androidx.compose.runtime.Composable
-import util.PackageScanUtil
-import java.lang.reflect.Method
-import java.util.*
+
 
 fun main() {
 
